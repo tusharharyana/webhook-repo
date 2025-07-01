dfb00abba9ee42383ada59afc0338360b6fe3962
tusharharyana <123148711+tusharharyana@users.noreply.github.com>
2025-07-01 20:01:27 +0530

    Add working Flask webhook with MongoDB atlas integration and GitHub event schema

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,19 @@
 from flask import Flask
 
 from app.webhook.routes import webhook
+from .extensions import mongo
 
 
 # Creating our flask app
 def create_app():
 
     app = Flask(__name__)
+    
+    # MongoDB configuration
+    app.config["MONGO_URI"] = "mongodb+srv://haryanatushar:UEKfsni52gJ3VXDz@cluster0.figgbyy.mongodb.net/github_webhooks?retryWrites=true&w=majority&appName=Cluster0"
+    
+    # Initialize Mongo with app    
+    mongo.init_app(app)
     
     # registering all the blueprints
     app.register_blueprint(webhook)
--- app/webhook/routes.py
+++ app/webhook/routes.py
@@ -1,7 +1,48 @@
-from flask import Blueprint, json, request
+from flask import Blueprint, request, jsonify
+from datetime import datetime
+from ..extensions import mongo
 
 webhook = Blueprint('Webhook', __name__, url_prefix='/webhook')
 
 @webhook.route('/receiver', methods=["POST"])
 def receiver():
-    return {}, 200
+    data = request.json
+    event_type = request.headers.get("X-GitHub-Event")
+
+    if not data or not event_type:
+        return jsonify({"error": "Missing data or event type"}), 400
+
+    try:
+        action = event_type.upper()
+
+        event = {
+            "request_id": None,
+            "author": None,
+            "action": action,
+            "from_branch": None,
+            "to_branch": None,
+            "timestamp": datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S UTC")
+        }
+
+        if action == "PUSH":
+            event["request_id"] = data.get("after")  # commit hash
+            event["author"] = data.get("pusher", {}).get("name")
+            event["to_branch"] = data.get("ref", "").split("/")[-1]
+
+        elif action == "PULL_REQUEST":
+            event["request_id"] = str(data.get("number"))  # PR ID
+            event["author"] = data.get("sender", {}).get("login")
+            event["from_branch"] = data.get("pull_request", {}).get("head", {}).get("ref")
+            event["to_branch"] = data.get("pull_request", {}).get("base", {}).get("ref")
+
+        elif action == "MERGE":
+            pass
+
+        else:
+            return jsonify({"message": f"Unhandled event type: {action}"}), 400
+
+        mongo.db.events.insert_one(event)
+        return jsonify({"message": "Event stored successfully"}), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
